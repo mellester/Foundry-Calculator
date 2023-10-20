ee704cfc46fc9aad7179b8c72503166ea9f7aa45
erkle64 <erkle64@gmail.com>
2023-10-20 21:41:35 +1300

    Update settings.js

--- settings.js
+++ settings.js
@@ -264,7 +264,6 @@ function renderMetallurgy(settings) {
     }
     if (metallurgyName !== spec.metallurgy.name) {
         spec.setMetallurgy(metallurgyName)
-        solver.findSubgraphs(spec)
     }
     var oldNode = document.getElementById("metallurgy")
     var cell = oldNode.parentNode
